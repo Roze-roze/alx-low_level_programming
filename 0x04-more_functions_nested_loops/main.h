2c3017349171f7320c3f880fa6a64b692619a5c6
Roze-roze <kassimroze@gmail.com>
2022-09-16 12:22:27 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,34 +1,17 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _isupper - checks for uppercase or lowercase
- *
- *   * @c: integer to check
- *
- *    * Return: 0
- *
- *     */
-
-
+ * _isupper - checks for uppercase and lowercase
+ * @c: integer to check
+ * Return: 0
+ */
 
 int _isupper(int c)
-
 {
-
-		if (c >= 'A' && c <= 'Z')
-
-				{
-
-							return (1);
-
-								}
-
-
-
-			return (0);
-
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
 }
 
