deb83ee76619ae1c1922fc0557b5f272d84be079
Roze-roze <kassimroze@gmail.com>
2022-09-20 10:32:28 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,12 @@
 /**
  * _puts - prints a string
  * @str: string to be printed
- * Return:string 
+ * Return: string 
  */
 void _puts(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
